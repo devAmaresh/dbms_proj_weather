e6a2602ac04680ed7b027beafb6447f72dcecb8b
devAmaresh <amprasad.2003@gmail.com>
2024-04-21 04:15:55 +0530

    done location

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -12,6 +12,7 @@ include("includes/authenticate.php");
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>Dashboard</title>
  <link rel="stylesheet" href="../src/output.css" />
+ <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
--- admin/user.php
+++ admin/user.php
@@ -9,6 +9,7 @@ include("includes/authenticate.php");
  <meta name="viewport" content="width=device-width, initial-scale=1.0">
  <title>User Manage</title>
  <link rel="stylesheet" href="../src/output.css" />
+ <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
 <?php
